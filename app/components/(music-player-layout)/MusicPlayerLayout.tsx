e9f61a6c55ef002a7a66dcb861ae5b13014da67e
Pencils0101001101001010 <sjuniversalpaint@gmail.com>
2026-04-24 16:19:10 +0200

    Adjusted for mobile

--- app/components/(music-player-layout)/MusicPlayerLayout.tsx
+++ app/components/(music-player-layout)/MusicPlayerLayout.tsx
@@ -244,7 +244,7 @@ const MusicPlayerLayout = () => {
                   xmlns="http://www.w3.org/2000/svg"
                   width="80"
                   fill="currentColor"
-                  className="bi bi-rewind"
+                  className="bi bi-rewind p-2"
                   viewBox="0 0 16 16"
                 >
                   <path d="M9.196 8 15 4.633v6.734zm-.792-.696a.802.802 0 0 0 0 1.392l6.363 3.692c.52.302 1.233-.043 1.233-.696V4.308c0-.653-.713-.998-1.233-.696z" />
@@ -254,7 +254,7 @@ const MusicPlayerLayout = () => {
                   xmlns="http://www.w3.org/2000/svg"
                   width="80"
                   fill="currentColor"
-                  className="bi bi-pause"
+                  className="bi bi-pause p-2"
                   viewBox="0 0 16 16"
                 >
                   <path d="M6 3.5a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-1 0V4a.5.5 0 0 1 .5-.5m4 0a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-1 0V4a.5.5 0 0 1 .5-.5" />
@@ -263,7 +263,7 @@ const MusicPlayerLayout = () => {
                   xmlns="http://www.w3.org/2000/svg"
                   width="80"
                   fill="currentColor"
-                  className="bi bi-fast-forward"
+                  className="bi bi-fast-forward p-2"
                   viewBox="0 0 16 16"
                 >
                   <path d="M6.804 8 1 4.633v6.734zm.792-.696a.802.802 0 0 1 0 1.392l-6.363 3.692C.713 12.69 0 12.345 0 11.692V4.308c0-.653.713-.998 1.233-.696z" />
