8282a711a2cbf9b6fcee1d8ec417ff7b792b418e
Pencils0101001101001010 <sjuniversalpaint@gmail.com>
2026-05-08 19:15:36 +0200

    Changed greeting message on first page in onboarding

--- app/profileStep/page.tsx
+++ app/profileStep/page.tsx
@@ -4,7 +4,7 @@ import "./style.css";
 function page() {
   return (
     <div className="mainContainer text-center">
-      <h1 className="heading">Aloe there! Ready to start?</h1>
+      <h1 className="heading">Your home just greener</h1>
       <p className="heading">Expert tips for happy plants.</p>
       <Link href="/finalScreen" className="btnStyle">
         next
