2c534371ff58e23a25e71c97b063ae6c66669e08
Pencils0101001101001010 <sjuniversalpaint@gmail.com>
2026-05-08 19:20:45 +0200

    centered final boarding screen

--- app/finalScreen/page.tsx
+++ app/finalScreen/page.tsx
@@ -3,7 +3,7 @@ import "./style.css";
 
 function page() {
   return (
-    <div className="mainContainer">
+    <div className="mainContainer text-center">
       <h1 className="heading">We're rooting for you.</h1>
       <p className="heading">Your green thumb starts here.</p>
       <Link href="/" className="btnStyle">
