8c2e99e643913fe03ad08f4b979a544ec236b484
Pencils0101001101001010 <sjuniversalpaint@gmail.com>
2026-06-15 19:27:52 +0200

    Added a coupon component

--- app/components/(CouponPage)/CouponPage.tsx
+++ app/components/(CouponPage)/CouponPage.tsx
@@ -1,9 +1,19 @@
-import React from "react";
+import "./styles.css";
 
 const CouponPage = () => {
   return (
-    <div>
-      <h1>Hey</h1>
+    <div className="coupon-page-container">
+      <div className="container-grid-layout">
+        <div className="text-area-grid">
+          <span className="coupon-header-style"> Save 30% on All Plants</span>
+          <span className="description-section">
+            Expand your collection with our high-quality plants. Limited time
+            only.
+          </span>
+          <span className="coupon-btn-style">Redeem Coupon</span>
+        </div>
+        <div className="box-background-style"></div>
+      </div>
     </div>
   );
 };
