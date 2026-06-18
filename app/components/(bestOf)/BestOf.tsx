c2bd2b844e2f649be6b2ce0f919c5aad0526abab
Pencils0101001101001010 <sjuniversalpaint@gmail.com>
2026-06-18 06:16:16 +0200

    completed best of component

--- app/components/(bestOf)/BestOf.tsx
+++ app/components/(bestOf)/BestOf.tsx
@@ -1,9 +1,22 @@
-import React from "react";
+import "./styles.css";
 
 const BestOf = () => {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="best-of-container">
+      <div className="main-box-container">
+        <div className="best-of-header-text">ARTIST OF THE YEAR</div>
+        <div className="description-for-best-of">
+          <div>Street artist of 2089 goes to @VAR!</div>
+          <div>Congratulations from the team here at writing on the wall.</div>
+          <div></div>
+        </div>
+        <div className="best-of-image-section">
+          <div className="best-of-img-1"></div>
+          <div className="best-of-img-2"></div>
+          <div className="best-of-img-3"></div>
+          <div className="best-of-img-4"></div>
+        </div>
+      </div>
     </div>
   );
 };
