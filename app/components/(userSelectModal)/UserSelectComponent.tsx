92bcb63b7b49dbc6fcf8dbbecef6fb7dd50a6b75
Pencils0101001101001010 <sjuniversalpaint@gmail.com>
2026-06-19 07:52:32 +0200

    User select modal

--- app/components/(userSelectModal)/UserSelectComponent.tsx
+++ app/components/(userSelectModal)/UserSelectComponent.tsx
@@ -3,7 +3,28 @@ import "./style.css";
 const UserSelectComponent = () => {
   return (
     <div className="user-select-modal-body">
-      <h1>Hey</h1>
+      <section className="user-select-modal-box">
+        <div className="select-modal-header">
+          {" "}
+          <div className="border-style-next-header"></div>Select your instrument
+        </div>
+        <div></div>
+        <div className="user-types">
+          <h2>Guitar</h2>
+          <div className="inner-user-t-select"></div>
+        </div>
+        <div className="user-types">
+          <h2>Bass</h2>
+          <div className="inner-user-t-select2"></div>
+        </div>
+        <div className="user-types">
+          <h2>Drums</h2>
+          <div className="inner-user-t-select3"></div>
+        </div>
+        <div className="next-btn-user text-end">
+          <div className="select-user-next-btn-stl">NEXT</div>
+        </div>
+      </section>
     </div>
   );
 };
