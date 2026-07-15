2647dd27c15d0137ae176c57fff9e074810696f3
Pencils0101001101001010 <sjuniversalpaint@gmail.com>
2026-07-15 15:41:27 +0200

    .

--- app/curated/page.tsx
+++ app/curated/page.tsx
@@ -4,12 +4,22 @@ export default function page() {
   return (
     <div className="page-body-container">
       <div className="image-grid-layout">
-        <div className="img1"></div>
-        <div className="img2"></div>
-        <div className="img3"></div>
-        <div className="img4"></div>
-        <div className="img5"></div>
-        <div className="img6"></div>
+        <div className="img1">
+          <div className="title-and-description">Table mountain cable way</div>
+        </div>
+        <div className="img2">
+          <div className="title-and-description">Because you visited V&A</div>
+        </div>
+        <div className="img3">
+          <div className="title-and-description">New Cafe @obz</div>
+        </div>
+        <div className="img4">
+          <div className="title-and-description">New Cafe @obz</div>
+        </div>
+        <div className="img5">
+          <div className="title-and-description">New Cafe @obz</div>
+        </div>
+        <div className="img6"> </div>
       </div>
     </div>
   );
